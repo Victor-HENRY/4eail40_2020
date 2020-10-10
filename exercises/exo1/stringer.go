ba50d85f49fdcea6031145ab0b6cd581ba5b2a92
Victor-HENRY <72656178+Victor-HENRY@users.noreply.github.com>
2020-10-10 14:46:36 +0200

    Update stringer.go

--- exercises/exo1/stringer.go
+++ exercises/exo1/stringer.go
@@ -2,7 +2,26 @@ package main
 
 import "fmt"
 
-// Implement types Rectangle, Circle and Shape
+//mon code
+type Shape interface {
+    String() string
+}
+
+type Rectangle struct {
+    Longueur  int64
+    Largeur int64
+}
+func (r Rectangle) String() string {
+    return fmt.Sprintf("Rectangle of width %d and length %d", r.Longueur, r.Largeur)
+}
+
+type Circle struct {
+    Rayon int64
+}
+func (c Circle) String() string {
+    return fmt.Sprintf("Circle of radius %d", c.Rayon)
+}
+
 
 func main() {
 	r := &Rectangle{2, 3}
