cd6a5c2615b918f8c53b60b17a42d3985b409ff5
Victor-HENRY <72656178+Victor-HENRY@users.noreply.github.com>
2020-10-28 14:39:48 +0100

    Add files via upload

--- exercises/exo2/reader.go
+++ exercises/exo2/reader.go
@@ -9,20 +9,6 @@ import (
 type spaceEraser struct {
 	r io.Reader
 }
-func (tr spaceEraser) Read(bufer []byte) (int, error) {
-    n, error := tr.r.Read(bufer)
-    var maliste []string
-    sli := strings.Split(string(bufer), "")
-    for _, el := range sli {
-        if el != " " {
-            maliste = append(maliste, el)
-        }
-    }
-    final := []byte(strings.Join(maliste, ""))
-    copy(bufer, final)
-    return n, error
-}
-
 
 func main() {
 	s := strings.NewReader("H e l l o w o r l d!")
